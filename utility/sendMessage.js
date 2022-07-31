7ac9c1a9e90f832ca4aa2c6c2bfaa28edab59eff
Szymon Swist <szymonswist97@gmail.com>
2022-07-31 17:15:23 +0200

    Change auto backup to work only in production

--- utility/sendMessage.js
+++ utility/sendMessage.js
@@ -1,12 +1,10 @@
-const DiscordJS = require("discord.js");
-
-const sendMessage = async (message, channelId, bot, timestamp) => {
-    const { client } = bot;
-    const channel = await client.channels.fetch(channelId);
-    const filename = "./" + timestamp + ".json";
-    await channel.send({
-        files: [filename]
-      });
+const sendMessage = async (channelId, bot, timestamp) => {
+  const { client } = bot;
+  const channel = await client.channels.fetch(channelId);
+  const filename = "./" + timestamp + ".json";
+  await channel.send({
+    files: [filename]
+  });
 }
 
 exports.sendMessage = sendMessage;
