7933bfe58cb97566e778dd79415f7f9193efe26c
Szymon Swist <szymonswist97@gmail.com>
2022-08-04 00:10:43 +0200

    Hotfix backup

--- bot.js
+++ bot.js
@@ -4,47 +4,54 @@ const { sendMessage } = require("./utility/sendMessage");
 require("dotenv").config();
 const fs = require("fs");
 
-
 const client = new DiscordJS.Client({
-  intents: ["GUILDS"]
+  intents: ["GUILDS"],
 });
 
 let bot = {
-  client
+  client,
 };
 
 client.slashcommands = new DiscordJS.Collection();
-client.loadSlashCommands = (bot, reload) => require("./handlers/slashcommands")(bot, reload);
+client.loadSlashCommands = (bot, reload) =>
+  require("./handlers/slashcommands")(bot, reload);
 client.loadSlashCommands(bot, false);
 
-client.on("interactionCreate", interaction => {
-  if (!interaction.isCommand()) return
-  if (!interaction.inGuild()) return interaction.reply("Komenda może być użyta tylko na serwerze");
+client.on("interactionCreate", (interaction) => {
+  if (!interaction.isCommand()) return;
+  if (!interaction.inGuild())
+    return interaction.reply("Komenda może być użyta tylko na serwerze");
 
   const slashcmd = client.slashcommands.get(interaction.commandName);
-  if (!slashcmd) return interaction.reply({ content: "Niepoprawna komenda", ephemeral: true });
+  if (!slashcmd)
+    return interaction.reply({
+      content: "Niepoprawna komenda",
+      ephemeral: true,
+    });
 
   if (slashcmd.perms && !interaction.member.permissions.has(slashcmd.perm))
-    return interaction.reply("Nie masz uprawnień do tej komendy")
+    return interaction.reply("Nie masz uprawnień do tej komendy");
 
   slashcmd.run(client, interaction);
 });
 
-client.on('ready', async () => {
+client.on("ready", async () => {
   console.log(`Logged in as ${client.user.tag}!`);
 
   //Backup DB to channel only in production
-  if(process.env.NODE_ENV !== 'development'){
+  if (process.env.NODE_ENV !== "development") {
     const data = await getAllData();
-    const timestamp = Date.now().toString()
-    fs.writeFile("./" + timestamp + ".json", data, (error) => {console.log(error)});
-    sendMessage(data, process.env.BACKUP_CHANNEL_ID, bot, timestamp);
+    const timestamp = Date.now().toString();
+    fs.writeFile("./" + timestamp + ".json", data, (error) => {
+      console.log(error);
+    });
+    sendMessage(process.env.BACKUP_CHANNEL_ID, bot, timestamp);
   }
 });
 
 client.login(process.env.BOT_TOKEN);
 
-process.on('uncaughtException', err => {
-  console.error('There was an uncaught error', err);
+process.on("uncaughtException", (err) => {
+  console.error("There was an uncaught error", err);
   process.exit(1);
 });
