2e026f3bf40e12b6d0f402e302d25895dc2c4539
Szymon Swist <szymonswist97@gmail.com>
2022-08-04 19:37:40 +0200

    Changed who can add recommendations

--- slashcommands/subcommands/recomendations/add.js
+++ slashcommands/subcommands/recomendations/add.js
@@ -13,49 +13,72 @@ const checkUserRoles = (recommender, recommended, negativeRecommend) => {
   const recommenderRank = recommender.rankData.rank;
 
   if (negativeRecommend)
-    if ([constants.RANKS.PODPULKOWNIK, constants.RANKS.PULKOWNIK, constants.RANKS.GENERAL].some(rank => rank === recommenderRank))
+    if (
+      [
+        constants.RANKS.PODPULKOWNIK,
+        constants.RANKS.PULKOWNIK,
+        constants.RANKS.GENERAL,
+      ].some((rank) => rank === recommenderRank)
+    )
       return { valid: true };
     else
-      return { valid: false, errorMessage: 'Nie możesz dawać ujemnych rekomendacji.' };
+      return {
+        valid: false,
+        errorMessage: "Nie możesz dawać ujemnych rekomendacji.",
+      };
 
   if (
     recommendedCorps !== constants.CORPS.KORPUS_OFICEROW &&
     recommendedCorps !== constants.CORPS.KORPUS_PODOFICEROW &&
     recommendedCorps !== constants.CORPS.KORPUS_STRZELCOW
-  ) return { valid: false, errorMessage: `<@${recommender.userID}> nie nalezy do żadnego korpusu` };
+  )
+    return {
+      valid: false,
+      errorMessage: `<@${recommender.userID}> nie nalezy do żadnego korpusu`,
+    };
 
   if (recommenderCorps === constants.CORPS.KORPUS_STRZELCOW)
-    return { valid: false, errorMessage: `Twój korpus nie może dawać rekomendacji` };
+    return {
+      valid: false,
+      errorMessage: `Twój korpus nie może dawać rekomendacji`,
+    };
 
   if (recommenderCorps === recommendedCorps) {
     if (
       recommenderCorps === constants.CORPS.KORPUS_OFICEROW &&
-      [constants.RANKS.PODPULKOWNIK, constants.RANKS.PULKOWNIK, constants.RANKS.GENERAL].some(rank => rank === recommenderRank)
-    ) return { valid: true };
-    return { valid: false, errorMessage: 'Nie możesz rekomendować osoby z tego samego korpusu.' };
+      [
+        constants.RANKS.PODPULKOWNIK,
+        constants.RANKS.PULKOWNIK,
+        constants.RANKS.GENERAL,
+      ].some((rank) => rank === recommenderRank)
+    )
+      return { valid: true };
+    return {
+      valid: false,
+      errorMessage: "Nie możesz rekomendować osoby z tego samego korpusu.",
+    };
   }
 
-  if (recommenderCorps === constants.CORPS.KORPUS_PODOFICEROW && recommendedCorps === constants.CORPS.KORPUS_STRZELCOW) {
-    if (recommendedRank === constants.RANKS.PLUTONOWY && recommenderRank === constants.RANKS.SIERZANT)
-      return { valid: false, errorMessage: 'Nie możesz rekomendować tej osoby' };
-    else
-      return { valid: true };
-  }
+  if (
+    recommendedRank === constants.RANKS.PLUTONOWY &&
+    recommenderRank === constants.CORPS.KORPUS_PODOFICEROW
+  )
+    return { valid: false, errorMessage: "Nie możesz rekomendować tej osoby" };
 
   if (recommenderCorps === constants.CORPS.KORPUS_OFICEROW)
     return { valid: true };
 
-  return { valid: false, errorMessage: 'Nie możesz rekomendować tej osoby' };
+  return { valid: false, errorMessage: "Nie możesz rekomendować tej osoby" };
 };
 
 const add = async (client, interaction) => {
   const memberRecommender = interaction.member;
-  const memberRecommended = interaction.options.getMember('gracz');
-  const negativeRecommend = interaction.options.getBoolean('ujemna');
-  const reason = interaction.options.getString('powod');
+  const memberRecommended = interaction.options.getMember("gracz");
+  const negativeRecommend = interaction.options.getBoolean("ujemna");
+  const reason = interaction.options.getString("powod");
 
   if (!memberRecommended || !memberRecommender)
-    return await interaction.editReply('Niepoprawny gracz');
+    return await interaction.editReply("Niepoprawny gracz");
 
   const memberRecommendedResult = await getUserData(memberRecommended.user.id);
   const memberRecommenderResult = await getUserData(memberRecommender.user.id);
@@ -64,43 +87,84 @@ const add = async (client, interaction) => {
   if (!memberRecommenderResult.valid)
     return interaction.editReply(memberRecommenderResult.errorMessage);
 
-  const checkUserRanks = await checkUserRoles(memberRecommenderResult.payLoad, memberRecommendedResult.payLoad, negativeRecommend);
+  const checkUserRanks = await checkUserRoles(
+    memberRecommenderResult.payLoad,
+    memberRecommendedResult.payLoad,
+    negativeRecommend
+  );
   if (!checkUserRanks.valid)
     return await interaction.editReply(checkUserRanks.errorMessage);
 
-  await interaction.editReply('Dodaje rekomendacje ...');
+  await interaction.editReply("Dodaje rekomendacje ...");
   if (negativeRecommend === true) {
-    const result = await recommendUser(memberRecommender.user.id, memberRecommended.user.id, reason, negativeRecommend);
-    if (!result.valid)
-      return await interaction.editReply(result.errorMessage);
+    const result = await recommendUser(
+      memberRecommender.user.id,
+      memberRecommended.user.id,
+      reason,
+      negativeRecommend
+    );
+    if (!result.valid) return await interaction.editReply(result.errorMessage);
 
     if (result.payLoad.rankData.promotion === true)
-      if (!checkPromotion(result.payLoad.rankData.rank, result.payLoad.rankData.corps, result.payLoad.rankData.currentNumber)) {
-        const updatedUser = await updateUser(memberRecommended.user.id, { $set: { 'rankData.promotion': false } });
+      if (
+        !checkPromotion(
+          result.payLoad.rankData.rank,
+          result.payLoad.rankData.corps,
+          result.payLoad.rankData.currentNumber
+        )
+      ) {
+        const updatedUser = await updateUser(memberRecommended.user.id, {
+          $set: { "rankData.promotion": false },
+        });
         if (!updatedUser.valid)
           return await interaction.editReply(updatedUser.errorMessage);
       }
 
-    await interaction.editReply(`<@${memberRecommended.user.id}> otrzymał ujemną rekomendacje od <@${memberRecommender.user.id}> powód:\n ${reason}`);
+    await interaction.editReply(
+      `<@${memberRecommended.user.id}> otrzymał ujemną rekomendacje od <@${memberRecommender.user.id}> powód:\n ${reason}`
+    );
   } else {
-    const result = await recommendUser(memberRecommender.user.id, memberRecommended.user.id, reason);
+    const result = await recommendUser(
+      memberRecommender.user.id,
+      memberRecommended.user.id,
+      reason
+    );
     if (!result.valid) {
-      if (result.errorMessage === `<@${memberRecommended.user.id}> już wcześniej otrzymał od Ciebie ${negativeRecommend ? 'ujemna' : ''} rekomendacje.`) {
+      if (
+        result.errorMessage ===
+        `<@${memberRecommended.user.id}> już wcześniej otrzymał od Ciebie ${
+          negativeRecommend ? "ujemna" : ""
+        } rekomendacje.`
+      ) {
         const message = await interaction.editReply(result.errorMessage);
-        return message.react('🍄').catch(err => console.error(err));
+        return message.react("🍄").catch((err) => console.error(err));
       }
 
       return await interaction.editReply(result.errorMessage);
     }
 
-    if (checkPromotion(result.payLoad.rankData.rank, result.payLoad.rankData.corps, result.payLoad.rankData.currentNumber)) {
-      const updatedUser = await updateUser(memberRecommended.user.id, { $set: { 'rankData.promotion': true } });
+    if (
+      checkPromotion(
+        result.payLoad.rankData.rank,
+        result.payLoad.rankData.corps,
+        result.payLoad.rankData.currentNumber
+      )
+    ) {
+      const updatedUser = await updateUser(memberRecommended.user.id, {
+        $set: { "rankData.promotion": true },
+      });
 
       if (!updatedUser.valid)
-        return repair(client, interaction, `<@${memberRecommender.user.id}> rekomenduje <@${memberRecommended.user.id}> - ${reason}\n<@${memberRecommended.user.id}> jest gotowy do awansu ale nie udało mu sie ustawić odpowiedniej flagi. \nNastępuje próba naprawy.\n`);
+        return repair(
+          client,
+          interaction,
+          `<@${memberRecommender.user.id}> rekomenduje <@${memberRecommended.user.id}> - ${reason}\n<@${memberRecommended.user.id}> jest gotowy do awansu ale nie udało mu sie ustawić odpowiedniej flagi. \nNastępuje próba naprawy.\n`
+        );
     }
 
-    await interaction.editReply(`<@${memberRecommender.user.id}> rekomenduje <@${memberRecommended.user.id}> - ${reason}`);
+    await interaction.editReply(
+      `<@${memberRecommender.user.id}> rekomenduje <@${memberRecommended.user.id}> - ${reason}`
+    );
   }
 };
 module.exports = { add };
