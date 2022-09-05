f017e5837f3c9d4b8da175703a1c64994ba49587
Qache <gortatm@gmail.com>
2022-09-05 18:24:42 +0200

    working on backups

--- database/doBackup.js
+++ database/doBackup.js
@@ -5,35 +5,22 @@ const { sendMessage } = require("../utility/sendMessage");
 
 const doBackup = async (bot) => {
     try {
-        await client.connect();
+        var session = client.startSession();
         const db = await database.listCollections().toArray();
-        await client.close();
+        const timestamp = Date.now().toString();
         db.forEach(async (collInfo) => {
-            await client.connect();
             const coll = await database.collection(collInfo.name).find().toArray();
-            console.log(coll);
-            await client.close();
+            const collName = collInfo.name.toString();
+            const json = JSON.stringify(coll, null, 2);
+            const fileName = collName + "_" + timestamp;
+            createJSONFile(json.toString(), fileName);
+            sendMessage(process.env.BACKUP_CHANNEL_ID, bot, fileName);
         });
-        
-        //return;
-        //const timestamp = Date.now().toString();
-        //let i = 0;
-        //db.forEach(async (collection) => {
-        //    const collName = collection.collectionName.toString();
-        //    console.log(i++, "_tutaj_", collection);
-        //    const coll = await database.collection(collName).find().toArray();
-        //    const json = JSON.stringify(coll, null, 2);
-        //    const fileName = collName + "_" + timestamp;
-        //    createJSONFile(json.toString(), fileName);
-        //    sendMessage(process.env.BACKUP_CHANNEL_ID, bot, fileName);
-        //});
-        //return { valid: true, message: "Back się powiódł" };
+        session.endSession();
     } catch (e) {
         console.error(e);
         //return { valid: false, message: "Błąd połączenia z bazą" };
-    }// finally {
-    //    await client.close();
-    //}
+    }
 }
 
 exports.doBackup = doBackup;
